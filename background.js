ef5f2733216c2e60d74105f72ab01c0beca8324b
Vermillion Li <prowizardjoey@gmail.com>
2025-11-08 13:30:12 -0800

    updated background.js to actually be able to read url (maybe) updated manifest.json to make this a background worker and give tab permissions

--- background.js
+++ background.js
@@ -1,11 +1,29 @@
 
-setInterval(getCurrentTab, 10)
+setInterval(getCurrentTab, 500)
 console.log("if you didn't see this line, the program didn't work oh no")
 
 async function getCurrentTab() {
-    let queryOptions = { active: true};
+    let queryOptions = {active: true, lastFocusedWindow: true};
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let tab = await chrome.tabs.query(queryOptions);
-    console.log("this is what tab return: " + tab)
-    return tab.toString()
+    chrome.tabs.query(queryOptions, ([tab]) => {
+        if (!tab) {
+            console.warn('No active tab found. Are you on the console perchance???');
+            return;
+        }
+        console.log(tab.url)
+    })
+
+    //if anyone DELETES DELETES this documentation below, I will NOT NOT be HAPPY
+    //this is a callback function
+    //chrome.tabs.query takes in queryOptions as a variable and returns the value as tabs
+    //the second parameter is a lambda [function] that uses tabs (returned from the function running with 1st parameter)
+    //hence, the second parameter is a function that runs after function finishes with the actual input (1st parameter)
+    //we MUST MUST MUST do this because otherwise JS might try return tabs.url BEFORE the query is actually complete
+        //lambda inside the function MAKES MAKES MAKES sure that we KNOW what tabs is before returning it
+
+
 }
+
+//= is assignment
+//== loose equality
+//=== strict equality
