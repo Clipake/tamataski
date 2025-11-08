aa8e47c00b5b50c7b7a10cfe0ca7d32c56da5321
Clipake <87284709+Clipake@users.noreply.github.com>
2025-11-08 12:27:13 -0800

    movement

--- scripts/pet.js
+++ scripts/pet.js
@@ -4,16 +4,44 @@ function renderPet(){
     // cssConnector.href = chrome.runtime.getURL('/assets/test.css')
     // document.head.appendChild(cssConnector)
 
-    const wrapper = document.createElement('div')
-    wrapper.className = "tamataski_pet"
-    document.body.appendChild(wrapper)
+    const pet = document.createElement('div')
+    pet.className = "tamataski_pet"
+    
+    document.body.appendChild(pet)
 
     
     const image_element = document.createElement("img")
     image_element.src = chrome.runtime.getURL('/assets/smile_0.png')
     image_element.className = "test_image"
-    wrapper.appendChild(image_element)
+    pet.appendChild(image_element)
+
     
+    let petPosition = 0
+    const loopContents = async function(){
+        console.log("move element")
+        petPosition -= 100
+        const move = [
+            {transform: `translateX(${petPosition}px)`}
+        ]
+        const timing = {
+            duration: 2000,
+            iterations: 1,
+            fill: "forwards"
+        }
+        animation = pet.animate(move, timing);
+        await animation.finished;
+        animation.commitStyles();
+        animation.cancel();
+    }
+
+    const loop = function(){
+        loopContents()
+    
+        let timeout = (Math.floor(Math.random() * 5) + 2)*1000
+        setTimeout(loop, timeout)
+    }
+
+    loop();
     
 }
 
