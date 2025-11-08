cc4cf5105cb8ade6c47e7523d8d293f77c785258
Clipake <87284709+Clipake@users.noreply.github.com>
2025-11-08 11:56:09 -0800

    css, icon, float

--- scripts/pet.js
+++ scripts/pet.js
@@ -1,11 +1,20 @@
 function renderPet(){
-    const element = document.createElement('p')
-    element.textContent = "hello world!"
+    // const cssConnector = document.createElement("link")
+    // cssConnector.rel = "stylesheet"
+    // cssConnector.href = chrome.runtime.getURL('/assets/test.css')
+    // document.head.appendChild(cssConnector)
 
-    const body = document.querySelector("body")
+    const wrapper = document.createElement('div')
+    wrapper.className = "tamataski_pet"
+    document.body.appendChild(wrapper)
+
     
-    document.insertBefore(element, body)
-    const img = document.createElement('')
+    const image_element = document.createElement("img")
+    image_element.src = chrome.runtime.getURL('/assets/smile_0.png')
+    image_element.className = "test_image"
+    wrapper.appendChild(image_element)
+    
+    
 }
 
 renderPet()
