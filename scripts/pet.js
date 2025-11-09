ebc20f6264a39b19cd99d325bdd0ec38ce1176f4
Clipake <87284709+Clipake@users.noreply.github.com>
2025-11-08 18:48:00 -0800

    cat

--- scripts/pet.js
+++ scripts/pet.js
@@ -17,10 +17,10 @@ function renderPet(){
     // pet.className = "tamataski_pet"
     // container.appendChild(pet)
 
-    // const randomNumBetween = function(from, to){
-    //     const between = to - from
-    //     return Math.floor(Math.random() * between) - Math.abs(from)
-    // }
+    const randomNumBetween = function(from, to){
+         const between = to - from
+         return Math.floor(Math.random() * between) - Math.abs(from)
+    }
 
     
     // let petPosition = 0
@@ -64,33 +64,102 @@ function renderPet(){
     
 
     const imageSize = 32
+    const scaledImageSize = 80;
     const spritesWidth = 11
     const spritesHeight = 53
 
-    let frameX = 0
-    let frameY = 0
-    
+   
+    let frameNumber = 0
+    let frameX = (frameNumber+1)%11 - 1
+    let frameY = Math.floor((frameNumber+1)/11)
+
     const context = canvas.getContext("2d")
+    context.imageSmoothingEnabled = false
+
     let image = new Image()
     image.src = chrome.runtime.getURL("/assets/cat 1.png")
-    image.onload = function(){
-        setInterval(function(){
-            const webimge = new WebImage('');
-            WebImage.width = 32
-            WebImage.height = 32
-            WebImage.setPosition()
-            context.clearRect(0, 0, canvas.height, canvas.width)
-            context.drawImage(image, frameX*imageSize, frameY*imageSize, 32, 32, 0, canvas.height-25, 64, 64)
 
-            frameX += 1
-            if (frameX >= spritesWidth){
-                frameX = 0
-                frameY += 1
-                if (frameY >= spritesHeight){
-                    frameY = 0
-                }
+
+    let currentAnimation = [1, 1]
+    const walk_right = [66, 73]
+    const walk_left = [77, 84]
+    const sit_front = [0, 0]
+    
+    let currentPetState = 0;
+    let petVelocity = 0;
+    let petPosition = 0;
+
+    const setAnimation = function(animation){
+        currentAnimation = animation
+        frameNumber = animation[0]
+    }
+
+    const petStates = [
+        {
+            set: function(){
+                setAnimation(sit_front)
+            },
+            run: function(){}
+        },
+        {
+            set: function(){
+                petVelocity = Math.random() > 0.5 ? -1 : 1
+                setAnimation(petVelocity == -1 ? walk_left : walk_right)
+            },
+
+            run: function(){
+                petPosition += petVelocity
             }
-        }, 500)
+        }
+    ]
+
+    const petStateEnum = {
+        "Idle": 0,
+        "Walk": 1,
+    }   
+
+
+    
+    
+    image.onload = function(){
+        currentAnimation = walk_right
+        frameNumber = currentAnimation[0]
+
+        const animateLoop = setInterval(async function(){
+            
+            if (frameNumber >= currentAnimation[1]){
+                frameNumber = currentAnimation[0]
+            }else{
+                frameNumber += 1
+            }
+        
+            frameX = (frameNumber+1)%11 - 1
+            frameY = Math.floor((frameNumber+1)/11)
+        }, 200)
+        
+        const fastLoop = setInterval(async function(){
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            context.drawImage(image, frameX*imageSize, frameY*imageSize, 32, 32, petPosition, canvas.height-scaledImageSize+10, scaledImageSize, scaledImageSize)
+        
+            petStates[currentPetState].run()
+
+            if (petPosition < 0 ||  petPosition > canvas.width-scaledImageSize){
+                petVelocity = -petVelocity
+                setAnimation(petVelocity == -1 ? walk_left : walk_right)
+            }
+
+        }, 30)
+
+        const doLoop = function(){
+            currentPetState = randomNumBetween(0, Object.keys(petStateEnum).length)
+            petStates[currentPetState].set()
+            console.log(currentPetState)
+            let timeout = (Math.floor(Math.random() * 5) + 2)*1000 //0-3 seconds
+            setTimeout(doLoop, timeout)
+        }
+
+        doLoop();
+
         
     }
     //loop();
